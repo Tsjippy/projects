6a91b2353b62c3fe8d3e42d86540757d076b30b9
Tsjippy <ewaldha@hotmail.com>
2024-11-22 13:08:52 +0100

    changed: removed anonymous functions

--- php/main.php
+++ php/main.php
@@ -3,6 +3,7 @@ namespace SIM\PROJECTS;
 use SIM;
 
 // Create the location custom post type 
-add_action('init', function(){
+add_action('init', __NAMESPACE__.'\loadAssets', 999);
+function loadAssets(){
 	SIM\registerPostTypeAndTax('project', 'projects');
-}, 999);
+}
