5c5150638404c408929d1be635e7e4d8d8bfb813
Tsjippy <ewaldha@hotmail.com>
2025-02-13 09:28:50 +0100

    changed: module hooks now include module slug

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -9,13 +9,8 @@ DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
 
 // check for dependicies
-add_filter('sim_submenu_description',  __NAMESPACE__.'\moduleDescription', 10, 2);
-function moduleDescription($description, $moduleSlug){
-	//module slug should be the same as the constant
-	if($moduleSlug != MODULE_SLUG)	{
-		return $description;
-	}
-
+add_filter('sim_submenu_projects_description',  __NAMESPACE__.'\moduleDescription');
+function moduleDescription($description){
 	ob_start();
 
 	?>
