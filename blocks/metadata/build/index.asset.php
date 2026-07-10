cbae9e06f2aa09554df7293938c407cc6993f924
Tsjippy <ewaldha@hotmail.com>
2026-07-10 15:55:42 +0200

    changed: block name

--- blocks/metadata/build/index.asset.php
+++ blocks/metadata/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a2c45fa8fed0ebc7120b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '63c3a34137347869bb82');
